17a2f67b0acf4145f1dfa772162b49edf8c3f2cf
Marcelo Clivio <97415092+mclivio@users.noreply.github.com>
2022-09-06 20:35:15 -0300

    navhost

--- app/src/main/java/com/donc/tmdbapp/MainActivity.kt
+++ app/src/main/java/com/donc/tmdbapp/MainActivity.kt
@@ -3,13 +3,12 @@ package com.donc.tmdbapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.remember
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.donc.tmdbapp.ui.theme.TmdbAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,6 +16,25 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TmdbAppTheme {
+                /* NAVEGACION
+                DOC: https://developer.android.com/jetpack/compose/navigation?hl=es-419
+                 */
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "movies_list")
+                {
+                    composable("movies_list") {
+                        //Todo: Composable que muestre listado de peliculas populares
+                    }
+                    composable(
+                        "movie_details/{movie_id}",
+                        arguments = listOf(navArgument("movie_id") { type = NavType.IntType })
+                    ) {
+                        val movie_id = remember {
+                            it.arguments?.getInt("movie_id")
+                        }
+                        //Todo: Composable que muestre detalles de una pelicula (género, idioma original, popularidad, fecha de estreno, entre otras).
+                    }
+                }
             }
         }
     }
