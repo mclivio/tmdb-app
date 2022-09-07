11f860b8a9c6f80068a74a471f09d0ffe76bd1dc
Marcelo Clivio <97415092+mclivio@users.noreply.github.com>
2022-09-07 19:37:43 -0300

    added provides funs for hilt

--- app/src/main/java/com/donc/tmdbapp/di/AppModule.kt
+++ app/src/main/java/com/donc/tmdbapp/di/AppModule.kt
@@ -1,12 +1,34 @@
 package com.donc.tmdbapp.di
 
+import com.donc.tmdbapp.data.remote.MovieApi
+import com.donc.tmdbapp.repository.MovieRepository
+import com.donc.tmdbapp.util.Constants.BASE_URL
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
     /* Aca se define como se proporciona instancias de determinados tipos para que hilt haga inyecciones
     Al instalarse en un singleton, estas dependencias estaran disponibles en toda la aplicacion. */
+    @Singleton
+    @Provides
+    fun provideMovieRepository(
+        api: MovieApi
+    ) = MovieRepository(api)
+
+    @Singleton
+    @Provides
+    fun provideMovieApi(): MovieApi{
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(MovieApi::class.java)
+    }
 }
---
+++ app/src/main/java/com/donc/tmdbapp/util/Constants.kt
@@ -0,0 +1,16 @@
+package com.donc.tmdbapp.util
+
+object Constants {
+    const val BASE_URL = "https://api.themoviedb.org/3/"
+
+    /* Normalmente no pondria una key en un archivo subido a git,
+    solo esta aca para ahorrarle trabajo a quien revise el challenge */
+    const val API_KEY = "b824d19beb5b509f8d73d9a06bc2aaa5"
+
+    const val PAGE_SIZE = 20
+
+    const val LANGUAGE = "es-ES"
+
+    //TODO: Implementar MovieEntry. Esta url va a ser necesaria ↓
+    const val IMAGE_URL = "https://image.tmdb.org/t/p/w500/"
+}
